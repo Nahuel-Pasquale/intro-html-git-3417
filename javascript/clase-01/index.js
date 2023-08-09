84c463018f509bd4277fa1f2374496820d753cd0
npasquale <nahuel.pasquale@medicus.com.ar>
2023-08-09 19:12:09 -0300

    clase - 1 - js

---
+++ javascript/clase-01/index.js
@@ -0,0 +1,43 @@
+// alert('hola mundo');
+// prompt('ingresá tu nombre');
+/*
+    Comentario de varias lineas
+*/
+
+
+// number: numeros enteros y decimales.
+// string: cadenas de texto
+// boolean: true o false
+// array: arreglos
+// object: objetos
+// null: no tiene valor
+// undefinded: no se ha definido un valor
+
+// var nombre = 'Nahuel';
+var age = 30;
+var job = 'developer';
+var eyeColor;
+var country = null;
+
+// console.log('nombre ==> ', typeof(nombre));
+// console.log('age ==> ', typeof(age));
+// console.log('job ==> ', typeof(job));
+// console.log('eyeColor ==> ', typeof(eyeColor));
+// console.log('country ==> ', typeof(country));
+
+
+// console.log('age ==> ', age);
+// console.log('age ==> ', typeof(age));
+
+// window.confirm('confirma tu compra')
+
+// let nombre = prompt('ingresá tu nombre');
+// let apellido = prompt('ingresá tu apellido');
+
+console.log(nombre);
+console.log(typeof(nombre))  ;
+
+// window.confirm('Asi que tu nombre es ' + nombre + ' ' + apellido + '?')
+
+// console.log('Hola mi nombre es: ' + nombre + 'y mi apellido es: ' + apellido)
+
